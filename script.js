987b9f766f19640fc0c762af82cb6b64cfc031ce
amitsuthar803 <amitsuthar803@gmail.com>
2022-05-23 12:09:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let interestPanel = document.querySelector("#interest");
+let yearlyinterestPanel = document.querySelector("#yearly-interest");
+//percentage function
+
+document.querySelector(".check").addEventListener("click", function () {
+  let percentage = document.querySelector(".percentage").value;
+  let amount = document.querySelector(".amount").value;
+
+  if (percentage <= 0 || amount <= 0) {
+    alert("please enter the Value in both field");
+  } else {
+    let total = (amount / 100) * percentage;
+    interestPanel.textContent = total;
+    yearlyinterestPanel.textContent = total * 12;
+  }
+});
+
+document.querySelector(".clear").addEventListener("click", function () {
+  document.querySelector(".percentage").value = 0;
+  document.querySelector(".amount").value = 0;
+  interestPanel.textContent = 0;
+  yearlyinterestPanel.textContent = 0;
+});
